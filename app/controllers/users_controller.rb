b5fd39a00c9269264d84993d9a157e044de4abde
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-02-13 15:22:39 -0500

    enable sessions in application_controller.rb,  fill out get and post login in users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,17 @@ class UsersController < ApplicationController
 
   #job of this route is to recieve the login form, find user, and log user in/create a session
   post '/login' do
-    binding.pry
+    #find and authenticate user, login user and redirect to user's landing page(dashboard) 
+    @user = User.find_by(email: params[:email])
+    if @user.authenticate(params[:password])
+      #log user in and redirect to dashboard/create a session
+      session[:user_id] = @user.id #actually whats logging user in 
+      puts session
+      redirect "users/#{@user.id}"
+    else
+      #tell user invalid credentials, redirect to login page 
+    end
+
   end 
 
 
@@ -14,5 +24,8 @@ class UsersController < ApplicationController
   get '/signup' do
 
   end 
-
+  #user show route
+  get '/users/:id' do 
+    "this will be the user show route"
+  end 
 end 

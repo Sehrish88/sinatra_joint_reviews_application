f0e72a05b40970e74d396788ed1d2a481ce04920
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-02-13 13:19:03 -0500

    add login functionality to users_controller.rb and adjust app/views/login.erb file accordingly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,11 @@ class UsersController < ApplicationController
     erb :login
   end 
 
+  #job of this route is to recieve the login form, find user, and log user in/create a session
+  post '/login' do
+    binding.pry
+  end 
+
 
 #routes needed for signup
   get '/signup' do

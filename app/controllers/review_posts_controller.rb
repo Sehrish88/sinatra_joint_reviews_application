37ee929a3ecf89ef471b1890efbb7d1bfeb64f50
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-02-22 18:06:51 -0500

    add review_posts_controller.rb file, add a review_post folder into views folder

---
+++ app/controllers/review_posts_controller.rb
@@ -0,0 +1,15 @@
+class ReviewPostsController < ApplicationController
+    #get review_posts/new to render a form to create new review post
+    get '/review_posts/new' do
+        erb :'review_posts/new'
+    end 
+
+    #post request to create new review post 
+    post '/review_posts' do 
+
+    end 
+
+    #show page for journal entry 
+
+    #index route for all journal entries 
+end 

0707d6dba31febd80f0dde97bd7422c07207b6dd
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-04-09 13:09:01 -0400

    add a comment in config/environment.rb file to explain the database connection

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,7 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
+#Establishes the connection to the database. Accepts a hash as input where the :adapter key must be specified with the name of a database adapter
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
